3b5be10c9b112cebfe1fe5bb9b976a33415c0acd
Marcel Kalużny <marcelkaluzny123@gmail.com>
2025-08-28 11:48:03 +0200

    finished implementing graph representation in the procedure

--- src/core/Procedure.ts
+++ src/core/Procedure.ts
@@ -1,9 +1,16 @@
-import EntryNode from "../nodes/EntryNode";
+import EntryNode from "../nodes/EntryNode.ts";
 import Node from "../nodes/Node.ts";
+
+type ConnectedNode = {
+    id: string,
+    connections: Array<string>,
+}
 
 export default class Procedure {
     private _nodes: Map<string, Node>;
     private _connections: Map<string, undefined>;
+
+    private _graph: Map<string, Array<ConnectedNode>>;
 
     private _entryNode: Node; 
 
@@ -11,8 +18,39 @@ export default class Procedure {
         this._nodes = new Map<string, Node>();
         this._connections = new Map<string, undefined>();
 
+        this._graph = new Map<string, Array<ConnectedNode>>();
+
         this._entryNode = new EntryNode();
-        this._nodes.set(this._entryNode.getID(), this._entryNode);
+        this.insertNode(this._entryNode);
+    }
+
+    public insertNode(node: Node): void {
+        const nodeID = node.getID();
+        this._nodes.set(nodeID, node);
+        this._graph.set(nodeID, []);
+    }
+
+    public connect(id1: string, id2: string, connDetails: undefined) {
+        if (!this._graph.has(id1)) throw new Error(`Node with ID - ${id1}, doesn't exist!`);
+        if (!this._graph.has(id2)) throw new Error(`Node with ID - ${id2}, doesn't exist!`);
+
+        const node1Connections = this._graph.get(id1)!;
+        const connectionID = crypto.randomUUID();
+        
+        let isFound = false;
+
+        node1Connections.forEach(node => {
+            if (node.id !== id2 ) return;
+            node.connections.push(connectionID);
+            isFound = true;
+        });
+
+        if (!isFound) node1Connections.push({ 
+            id: id2, 
+            connections: [connectionID] 
+        });
+
+        this._connections.set(connectionID, connDetails);
     }
 
     public getNodes(): Node[] {
